710fafa41cbb21d20057d1e2f316432b4d21ef07
Mohammed Abbas Ali <abbasdaniyaal@gmail.com>
2026-07-07 14:41:38 +0530

    Final CI/CD test2

--- app.py
+++ app.py
@@ -4,7 +4,7 @@ app = Flask(__name__)
 
 @app.route("/")
 def home():
-    return "Hello from Kubernetes CI/CD Pipeline! By Abbas Ali, back to work "
+    return "Hello from Kubernetes CI/CD Pipeline! By Abbas Ali, back to work to devops openshift  "
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=5000)
