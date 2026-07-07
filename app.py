258dc8a5a69653c855497e7d29271bf96da07cae
Mohammed Abbas Ali <abbasdaniyaal@gmail.com>
2026-07-07 13:58:39 +0530

    Update application message2

--- app.py
+++ app.py
@@ -4,7 +4,7 @@ app = Flask(__name__)
 
 @app.route("/")
 def home():
-    return "Hello from Kubernetes CI/CD Pipeline! By Abbas Ali "
+    return "Hello from Kubernetes CI/CD Pipeline! By Abbas Ali Devops Junior "
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=5000)
